fd6af122a47acf4d8e1137cc1b25e8f3361cd295
Dieter Spillmann <dspillmann@arris.com>
2015-05-06 13:23:36 -0700

    local changes

--- Music/Project/src/ntest.py
+++ Music/Project/src/ntest.py
@@ -1,12 +1,40 @@
 import os
-startpath ="/Users/dieter"
-fullpath="/Users/dieter/Music/a/bla/xxx/1-dddd.mp3"
-print(fullpath)
-rootpath=os.path.dirname(fullpath)
-mypath=os.path.basename(fullpath)
-while os.path.dirname(rootpath) != startpath:
-    rootpath=os.path.dirname(rootpath)
-    mypath=os.path.join(os.path.basename(rootpath),mypath)
-    print(mypath)
- 
+import audiotools
+import sys, getopt
+splaylist = ''
+source_path = ''
+dest_path =''
+try:
+    #print (sys.argv)
+    myoptions, myargs = getopt.getopt(sys.argv[1:],"p:s:d:a:")
+except getopt.GetoptError as e:
+    print (str(e))
+    print("Usage: %s -s" % sys.argv[0])
+    sys.exit(2)
+for o, a in myoptions:
+        if o == '-s':
+            if a != '':
+                source_path = a
+            else:
+                source_path=input('Path to source folder? ')
+        elif o == '-d':
+            if a != '':
+                dest_path =a
+            else:
+                dest_path=input('Path to destination folder? ')
+        elif o == '-a':
+            artist = a
+            print(artist)
+            print (get_initial(artist))
+        elif o == '-p':
+            playlist = a 
+            #print(playlist)
+            if os.path.isdir(playlist):
+                print (playlist ,"exists and will be overwritten")
+    
 
+print(audiotools.VERSION)
+files = audiotools.open(source_path)
+print(files)
+for file in files:
+    print(file.track_metadata)
